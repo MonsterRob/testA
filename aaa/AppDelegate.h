9d22f3f94e7b2a2759772cecd08a7f47e22de2f9
MonsterRob <wmwwzhao@163.com>
2016-10-11 14:44:03 +0800

    first commit

---
+++ aaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaa
+//
+//  Created by 王召洲 on 2016/10/11.
+//  Copyright © 2016年 wyzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaa/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  aaa
+//
+//  Created by 王召洲 on 2016/10/11.
+//  Copyright © 2016年 wyzc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <QuartzCore/QuartzCore.h>
+#import <CoreGraphics/CoreGraphics.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
